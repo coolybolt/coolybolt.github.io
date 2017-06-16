e37f697612b52d1cd16460dbde985c0360d616ef
coolybolt <raijukage@gmail.com>
2017-06-16 13:21:05 -0600

    Add files via upload

---
+++ Web design-D/thing.java
@@ -0,0 +1,7 @@
+document.onkeydown = function (e) {
+  e = e || window.event;
+  switch (e.which || e.keyCode) {
+        case 13 : //Your Code Here (13 is ascii code for 'ENTER')
+            break;
+  }
+};
